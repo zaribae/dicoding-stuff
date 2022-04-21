3b487f2eec5089badabcc2e29663af8731270ec0
Zari Bae <ahmadazzhari@gmail.com>
2022-04-21 21:05:39 +0700

    hapi plugin added

--- notes-app-back-end/src/api/notes/handler.js
+++ notes-app-back-end/src/api/notes/handler.js
@@ -1,10 +1,20 @@
+const ClientError = require('../../exceptions/ClientError')
+
 class NotesHandler {
-  constructor (service) {
+  constructor (service, validator) {
     this._service = service
+    this._validator = validator
+
+    this.postNoteHandler = this.postNoteHandler.bind(this)
+    this.getNotesHandler = this.getNotesHandler.bind(this)
+    this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this)
+    this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this)
+    this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this)
   }
 
   postNoteHandler (request, h) {
     try {
+      this._validator.validateNotePayload(request.payload)
       const { title = 'untitled', body, tags } = request.payload
 
       const noteId = this._service.addNote({ title, body, tags })
@@ -16,15 +26,25 @@ class NotesHandler {
           noteId
         }
       })
-
       response.code(201)
       return response
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.'
       })
-      response.code(400)
+      response.code(500)
+      console.error(error)
       return response
     }
   }
@@ -50,17 +70,29 @@ class NotesHandler {
         }
       }
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.'
       })
-      response.code(404)
+      response.code(500)
+      console.error(error)
       return response
     }
   }
 
   putNoteByIdHandler (request, h) {
     try {
+      this._validator.validateNotePayload(request.payload)
       const { id } = request.params
 
       this._service.editNoteById(id, request.payload)
@@ -70,11 +102,22 @@ class NotesHandler {
         message: 'Catatan berhasil diperbarui'
       }
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: error.message
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.'
       })
-      response.code(404)
+      response.code(500)
+      console.error(error)
       return response
     }
   }
@@ -83,16 +126,28 @@ class NotesHandler {
     try {
       const { id } = request.params
       this._service.deleteNoteById(id)
+
       return {
         status: 'success',
         message: 'Catatan berhasil dihapus'
       }
     } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // Server ERROR!
       const response = h.response({
-        status: 'fail',
-        message: 'Catatan gagal dihapus. Id tidak ditemukan'
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.'
       })
-      response.code(404)
+      response.code(500)
+      console.error(error)
       return response
     }
   }
--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,10 +1,10 @@
 const Hapi = require('@hapi/hapi')
 const notes = require('./api/notes')
 const NotesService = require('./services/inMemory/NotesService')
+const NotesValidator = require('./validator/notes')
 
 const init = async () => {
   const notesService = new NotesService()
-
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -18,7 +18,8 @@ const init = async () => {
   await server.register({
     plugin: notes,
     options: {
-      service: notesService
+      service: notesService,
+      validator: NotesValidator
     }
   })
 
